8d91f29c839f241c65a5901018b258c8482a1f38
hibana2077 <hibana2077@gmail.com>
2024-07-09 00:19:54 +0800

    chore: Ignore ragtest.zip in .gitignore and update model.py with config changes

--- src/lab/model.py
+++ src/lab/model.py
@@ -9,13 +9,13 @@ class LTSFLinear(nn.Module):
     """
     def __init__(self, configs):
         super(LTSFLinear, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
+        self.seq_len = configs['seq_len']
+        self.pred_len = configs['pred_len']
         
         # Use this line if you want to visualize the weights
         # self.Linear.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-        self.channels = configs.enc_in
-        self.individual = configs.individual
+        self.channels = configs['enc_in']
+        self.individual = configs['individual']
         if self.individual:
             self.Linear = nn.ModuleList()
             for i in range(self.channels):
