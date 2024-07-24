0dd47bd8ddbe1ef95e97e4f8a0a5d216e9e7c03b
hibana2077 <hibana2077@gmail.com>
2024-07-24 22:38:45 +0800

    feat: Add basic_ltsf_linear_wd10 model results to JSON file

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-<!--
- * @Author: hibana2077 hibana2077@gmail.com
- * @Date: 2024-06-08 20:29:39
- * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2024-06-09 10:07:36
- * @FilePath: \do-shi-weather-you\README.md
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 # 都是 weather 你
 
 <p align="center">
---
+++ src/lab/results/result.py
@@ -0,0 +1,74 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load JSON data
+file_paths = [
+    './basic_lstm_wd10.json',
+    './basic_lstm_wd60.json',
+    './WeatherModel_Mix_wd10.json',
+    './WeatherModel_Mix_wd60.json',
+    './basic_ltsf_linear_wd10.json',
+    './basic_ltsf_linear_wd60.json'
+]
+
+data = {}
+
+for file_path in file_paths:
+    with open(file_path, 'r') as file:
+        content = json.load(file)
+        model_name = content['model_name']
+        data[model_name] = {
+            'train_loss': content['train_loss'],
+            'test_loss': content['test_loss']
+        }
+
+# Plotting the loss curves
+plt.figure(figsize=(15, 10))
+
+for model_name, losses in data.items():
+    plt.plot(losses['train_loss'], label=f'{model_name} Train Loss')
+    plt.plot(losses['test_loss'], label=f'{model_name} Test Loss')
+
+plt.xlabel('Epochs')
+plt.ylabel('Loss (MSE)')
+plt.title('Loss Curves for Different Models')
+plt.legend()
+plt.grid(True)
+plt.savefig('./loss_curves.png')
+
+# Calculate the error range for all models
+error_ranges = {}
+
+for model_name, losses in data.items():
+    mse = np.min(losses['test_loss'])
+    mae = np.sqrt(mse)
+    error_ranges[model_name] = {
+        'MSE': mse,
+        'MAE': mae
+    }
+
+# Prepare data for plotting
+labels = list(error_ranges.keys())
+mse_values = [error_ranges[model]['MSE'] for model in labels]
+mae_values = [error_ranges[model]['MAE'] for model in labels]
+
+# Plotting the error ranges
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots(figsize=(15, 10))
+rects1 = ax.bar(x - width/2, mse_values, width, label='MSE')
+rects2 = ax.bar(x + width/2, mae_values, width, label='± MAE')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xlabel('Models')
+ax.set_ylabel('Error')
+ax.set_title('Error Ranges for Different Models')
+ax.set_xticks(x)
+ax.set_xticklabels(labels, rotation=45, ha='right')
+ax.legend()
+
+fig.tight_layout()
+plt.grid(True)
+plt.savefig('./error_ranges.png')
